dd28cd256f4979dc2a0d37cb911aa298d07bddef
unknown <hrynkiv13@gmail.com>
2020-04-07 14:37:34 +0300

    version 0.1

---
+++ FuckSize/FuckSize/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace FuckSize
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainInput = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileBtn = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileBtn = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitBtn = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.FontBtn = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainInput
+            // 
+            this.mainInput.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.mainInput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainInput.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.mainInput.Location = new System.Drawing.Point(0, 24);
+            this.mainInput.Multiline = true;
+            this.mainInput.Name = "mainInput";
+            this.mainInput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.mainInput.Size = new System.Drawing.Size(800, 426);
+            this.mainInput.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFileBtn,
+            this.saveFileBtn,
+            this.exitBtn});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openFileBtn
+            // 
+            this.openFileBtn.Name = "openFileBtn";
+            this.openFileBtn.Size = new System.Drawing.Size(180, 22);
+            this.openFileBtn.Text = "Open";
+            this.openFileBtn.Click += new System.EventHandler(this.openFileBtn_Click);
+            // 
+            // saveFileBtn
+            // 
+            this.saveFileBtn.Name = "saveFileBtn";
+            this.saveFileBtn.Size = new System.Drawing.Size(180, 22);
+            this.saveFileBtn.Text = "Save";
+            this.saveFileBtn.Click += new System.EventHandler(this.saveFileBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(180, 22);
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FontBtn});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.editToolStripMenuItem.Text = "View";
+            // 
+            // FontBtn
+            // 
+            this.FontBtn.Name = "FontBtn";
+            this.FontBtn.Size = new System.Drawing.Size(180, 22);
+            this.FontBtn.Text = "Font";
+            this.FontBtn.Click += new System.EventHandler(this.FontBtn_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.mainInput);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "FuckSize Editor";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox mainInput;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFileBtn;
+        private System.Windows.Forms.ToolStripMenuItem saveFileBtn;
+        private System.Windows.Forms.ToolStripMenuItem exitBtn;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem FontBtn;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+    }
+}
+
---
+++ FuckSize/FuckSize/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FuckSize
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openFileBtn_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "C++ source files (*.cpp)|*.cpp|JavaScript (*.js)|*.js|All files (*.*)|*.*";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamReader reader = new StreamReader(openFileDialog1.FileName, Encoding.Default))
+                {
+                    mainInput.Text = reader.ReadToEnd();
+                }
+            }
+        }
+
+        private void saveFileBtn_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "C++ source files (*.cpp)|*.cpp|JavaScript (*.js)|*.js|All files (*.*)|*.*";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter writer = new StreamWriter(saveFileDialog1.FileName, false, Encoding.Default))
+                {
+                    writer.Write(mainInput.Text);
+                }
+            }
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void FontBtn_Click(object sender, EventArgs e)
+        {
+            if (fontDialog1.ShowDialog() == DialogResult.OK)
+            {
+                mainInput.Font = fontDialog1.Font;
+            }
+        }
+    }
+}
